703a1f6f23758c72f62df6f68425756cc726f04f
Yoga Hapriana <thenelse@rocketmail.com>
2012-12-29 01:23:00 +0700

    Deleting unused files

--- app/models/a_kota.rb
+++
@@ -1,4 +0,0 @@
-class AKota < ActiveRecord::Base
-  self.table_name = "a_kotas"
-  attr_accessible :id_negara, :id_provinsi, :kode, :nama, :simbol, :updated_by
-end
--- test/unit/a_kotum_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class AKotumTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
