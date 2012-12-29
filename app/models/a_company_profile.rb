b7acdb6c50af7aa8106be84e156114f04b660f9f
Yoga Hapriana <thenelse@rocketmail.com>
2012-12-29 21:11:28 +0700

    Fixing company profile validation

--- app/models/a_company_profile.rb
+++ app/models/a_company_profile.rb
@@ -1,10 +1,17 @@
 class ACompanyProfile < ActiveRecord::Base
   attr_accessible :alamat_01, :alamat_02, :alamat_03, :alamat_title, :contact_person, :email, :fax, :homepage, :kode, :nama_company, :telepon
-  validates :alamat_01, :alamat_02, :alamat_03, :alamat_title, :contact_person, :email, :fax, :kode, :nama_company, :telepon, presence: true  
-  validates :kode, uniqueness: { message: " harus unik" }, length: { maximum: 1, message: " terlalu panjang, maksimal 1 karakter" }
+  validates :alamat_01, :alamat_02, :alamat_03, :alamat_title, :contact_person, :email, :fax, :nama_company, :telepon, presence: true  
+#  validates :kode, uniqueness: { message: " harus unik" }, length: { maximum: 1, message: " terlalu panjang, maksimal 1 karakter" }
   validates :nama_company, :contact_person, length: { maximum: 50, message: " terlalu panjang, maksimal 50 karakter" }
   validates :alamat_title, :alamat_01, :alamat_02, :alamat_03, length: { maximum: 90, message: " terlalu panjang, maksimal 90 karakter" }
   validates :telepon, :fax, :email, :homepage, length: { maximum: 30, message: " terlalu panjang, maksimal 30 karakter" }
   validates :telepon, :fax, numericality: { message: " harus nomor" }
   validates :email, email_format: { message: " harus berformat nama@email.com" }
+
+  before_create :get_last_kode
+
+  def get_last_kode
+    last_a_company_profile = ACompanyProfile.last
+    self.kode = last_a_company_profile.blank? ? 1 : (last_a_company_profile.kode.to_i + 1)
+  end
 end
--- app/models/a_satuan.rb
+++ app/models/a_satuan.rb
@@ -1,6 +1,5 @@
 class ASatuan < ActiveRecord::Base
   attr_accessible :nama, :no_urut, :simbol, :updated_by
-
   validates :nama, :no_urut, :simbol, :updated_by, presence: true
   validates :simbol, uniqueness: true
 end
