dc607063aa086bc9d057e2e6c9f6ac2f4d870f85
Yoga Hapriana <thenelse@rocketmail.com>
2013-01-01 16:08:55 +0700

    Modifying app/models/a_negara.rb to handling blank updated_by

--- app/models/a_negara.rb
+++ app/models/a_negara.rb
@@ -1,3 +1,9 @@
 class ANegara < ActiveRecord::Base
   attr_accessible :kode, :nama, :simbol, :updated_by
+  before_create :check_updater
+  before_save :check_updater
+
+  def check_updater
+    self.updated_by = self.updated_by.blank? ? "ANONYMOUS" : self.updated_by
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -41,6 +41,15 @@ puts "Seeding AKemasan..."
   })
 end
 
+puts "Seeding ANegara..."
+1.upto(10) do |number|
+  ANegara.create({
+    kode: "n#{number}",
+    nama: "Nama #{number}",
+    simbol: "s#{number}"    
+  })
+end
+
 AdminMsUser.delete_all
 AdminMsUser.create(:login_name =>'0331960121', :id_group =>'00', :user_name =>'AIRMANSENA', :password =>'1111', :password_confirmation =>'1111', :limitlogin =>99, :tglcreate =>'2008-01-01', :tglpasswordexpired =>'2900-01-01', :tgllastlogin =>'2008-01-04', :pin =>'111111', :created_at =>'1899-12-30', :updated_at =>'1899-12-30', :status =>0)
 AdminMsUser.create(:login_name =>'herman', :id_group =>'00', :user_name =>'herman', :password =>'1111', :password_confirmation =>'1111', :limitlogin =>1, :tglcreate =>'2012-09-24', :tglpasswordexpired =>'2012-09-24', :tgllastlogin =>'2012-09-24', :pin =>'1', :created_at =>'2012-09-24', :updated_at =>'2012-09-24', :status =>1)
