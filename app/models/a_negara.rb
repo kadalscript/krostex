128366f1f65d656b41c873f598c02a25b629f797
Yoga Hapriana <thenelse@rocketmail.com>
2013-01-01 18:01:46 +0700

    Fixing bugs in a_negaras

--- app/models/a_negara.rb
+++ app/models/a_negara.rb
@@ -1,7 +1,8 @@
 class ANegara < ActiveRecord::Base
   attr_accessible :kode, :nama, :simbol, :updated_by
   validates :kode, :simbol, presence: true, length: { maximum: 5, message: TOO_LONG_CHARACTER }
-  validates :nama, :updated_by, presence: true, length: { maximum: 30, message: TOO_LONG_CHARACTER }
+  validates :nama, presence: true, length: { maximum: 30, message: TOO_LONG_CHARACTER }
+  validates :updated_by, length: { maximum: 30, message: TOO_LONG_CHARACTER }
   before_create :check_updater
   before_save :check_updater
 
