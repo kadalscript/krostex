f4657ddd1313a2eab2900c4ba0476f8fdc85e840
Yoga Hapriana <thenelse@rocketmail.com>
2013-01-01 23:25:09 +0700

    Change column type of id_negara and id_provinsi in cities table

--- db/migrate/20121228085811_create_a_cities.rb
+++ db/migrate/20121228085811_create_a_cities.rb
@@ -4,8 +4,8 @@ class CreateACities < ActiveRecord::Migration
       t.string :kode, limit: 5
       t.string :nama, limit: 30
       t.string :simbol, limit: 5
-      t.string :id_negara, limit: 5
-      t.string :id_provinsi, limit: 5
+      t.integer :id_negara, limit: 5
+      t.integer :id_provinsi, limit: 5
       t.string :updated_by, limit: 30
 
       t.timestamps
