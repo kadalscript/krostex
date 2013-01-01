baba0755cb8f3d1d202f11069d4b7b2eefc4eddf
Yoga Hapriana <thenelse@rocketmail.com>
2013-01-01 23:35:36 +0700

    Adding function check updater to a_city.rb

--- app/models/a_city.rb
+++ app/models/a_city.rb
@@ -3,4 +3,10 @@ class ACity < ActiveRecord::Base
   validates :kode, :simbol, presence: true, length: { maximum: 5, message: TOO_LONG_CHARACTER }
   validates :nama, presence: true, length: { maximum: 30, message: TOO_LONG_CHARACTER }
   validates :updated_by, length: { maximum: 30, message: TOO_LONG_CHARACTER }
+  before_create :check_updater
+  before_save :check_updater
+
+  def check_updater
+    self.updated_by = self.updated_by.blank? ? "ANONYMOUS" : self.updated_by
+  end
 end
