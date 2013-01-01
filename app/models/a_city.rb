95198219d75fa243fc31f94bc1bf4e9eeca5ca15
Yoga Hapriana <thenelse@rocketmail.com>
2013-01-01 22:47:30 +0700

    Create validation for app/models/a_kota.rb

--- app/models/a_city.rb
+++ app/models/a_city.rb
@@ -1,3 +1,6 @@
 class ACity < ActiveRecord::Base
   attr_accessible :id_negara, :id_provinsi, :kode, :nama, :simbol, :updated_by
+  validates :kode, :simbol, presence: true, length: { maximum: 5, message: TOO_LONG_CHARACTER }
+  validates :nama, presence: true, length: { maximum: 30, message: TOO_LONG_CHARACTER }
+  validates :updated_by, length: { maximum: 30, message: TOO_LONG_CHARACTER }
 end
