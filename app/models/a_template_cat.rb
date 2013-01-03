ee374edc934a011cd12bc2add563486c30bceea4
Yoga Hapriana <thenelse@rocketmail.com>
2013-01-03 23:44:01 +0700

    Create method for updating default updated_by

--- app/models/a_template_cat.rb
+++ app/models/a_template_cat.rb
@@ -3,10 +3,14 @@ class ATemplateCat < ActiveRecord::Base
   validates :kategori, :simbol, :st_progress, presence: true, length: { maximum: 20, message: TOO_LONG_CHARACTER }
   validates :nama, presence: true, length: { maximum: 30, message: TOO_LONG_CHARACTER }
   validates :updated_by, length: { maximum: 30, message: TOO_LONG_CHARACTER }
-  before_create :updating_status1
-  before_save :updating_status1
+  before_create :updating_status1, :check_updater
+  before_save :updating_status1, :check_updater
 
   def updating_status1
     self.status1 = st_progress == 'ACTIVE' ? 0 : 99
+  end
+
+  def check_updater
+    self.updated_by = self.updated_by.blank? ? "ANONYMOUS" : self.updated_by
   end
 end
