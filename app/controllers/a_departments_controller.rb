dd2e103346550728b5056dfa73082e743732b0c4
Yoga Hapriana <thenelse@rocketmail.com>
2013-01-05 03:36:48 +0700

    Refactoring a_departments_controller.rb

--- app/controllers/a_departments_controller.rb
+++ app/controllers/a_departments_controller.rb
@@ -1,6 +1,10 @@
 class ADepartmentsController < ApplicationController
-  before_filter :attribute, only: [:new, :show, :edit, :destroy_show]
+  before_filter :attributes, only: [:new, :show, :edit, :destroy_show, :create, :update]
   before_filter :find_a_department_by_id, only: [:show, :edit, :update, :destroy, :destroy_show]
+  before_filter :get_miscellaneous
+
+  @@title = 'department'
+  @@table_name = ADepartment.table_name
 
   def index
     @a_departments = ADepartment.page(params[:page]).per(5)
@@ -11,30 +15,26 @@ class ADepartmentsController < ApplicationController
   end
 
   def show
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @a_department }
-    end
+    common_form(@@table_name, @@title, @a_department)
   end
 
   def new
     @a_department = ADepartment.new
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @a_department }
-    end
+    common_form(@@table_name, @@title, @a_department)
   end
 
-  def edit; end
+  def edit
+    common_form(@@table_name, @@title, @a_department)
+  end
 
   def create
     @a_department = ADepartment.new(params[:a_department])
     respond_to do |format|
       if @a_department.save
-        format.html { redirect_to @a_department, notice: 'Data berhasil dibuat' }
+        format.html { redirect_to @a_department, notice: SUCCESFFULLY_SAVE_DATA }
         format.json { render json: @a_department, status: :created, location: @a_department }
       else
-        format.html { render action: "new" }
+        format.html { common_form(@@table_name, @@title, @a_department) }
         format.json { render json: @a_department.errors, status: :unprocessable_entity }
       end
     end
@@ -43,10 +43,10 @@ class ADepartmentsController < ApplicationController
   def update
     respond_to do |format|
       if @a_department.update_attributes(params[:a_department])
-        format.html { redirect_to @a_department, notice: 'Data berhasil di update' }
+        format.html { redirect_to @a_department, notice: SUCCESSFULLY_UPDATE_DATA }
         format.json { head :no_content }
       else
-        format.html { render action: "edit" }
+        format.html { common_form(@@table_name, @@title, @a_department) }
         format.json { render json: @a_department.errors, status: :unprocessable_entity }
       end
     end
@@ -55,26 +55,39 @@ class ADepartmentsController < ApplicationController
   def destroy
     @a_department.destroy
     respond_to do |format|
-      format.html { redirect_to a_departments_url, notice: 'Data berhasil dihapus' }
+      format.html { redirect_to a_departments_url, notice: SUCCESSFULLY_DELETE_DATA }
       format.json { head :no_content }
     end
   end
 
-  def destroy_show; end
+  def destroy_show
+    common_form(@@table_name, @@title, @a_department)
+  end
 
   def search
-    query = {}
-    ADepartment.column_names.each { |column_name| query.merge!({ column_name => params[column_name] }) if params[column_name].present? }
-    @a_departments = ADepartment.where(query).page(params[:page]).per(5)
-    render template: "a_departments/index"
+    queries = {}
+    ADepartment.column_names.each { |column_name| queries.merge!({ column_name => params[column_name] }) if params[column_name].present? }
+    @a_departments = ADepartment.where(queries).page(params[:page]).per(12)
+    notifications = ""
+    queries.each_pair { |key, value| notifications += "#{ADepartment.human_attribute_name(key).titleize} = \"#{value}\"<br />" }
+    flash.now[:notice] = "Hasil pencarian :<br /> #{notifications}".html_safe
+    render template: "#{@@table_name}/index"
   end
 
 private
   def find_a_department_by_id
     @a_department = ADepartment.find_by_id(params[:id])
     if @a_department.blank?
-      flash[:alert] = "Data tidak ditemukan"
-      redirect_to a_departments_path
+      respond_to do |format|
+        format.html { redirect_to a_departments_path, alert: NOT_FOUND_DATA }
+        format.json { head :no_content }
+      end
     end
+  end
+
+  def get_miscellaneous
+    @title = @@title
+    @hidden_columns = ["id", "created_at"]
+    @read_only_attributes = { readonly: true, disabled: 'disabled', style: 'width: 300px;' }
   end
 end
