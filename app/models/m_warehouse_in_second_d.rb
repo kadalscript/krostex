9b3065cd3b49f4df9e65b2a927df11f2273076b7
Yoga Hapriana <thenelse@rocketmail.com>
2013-01-16 00:22:19 +0700

    Calculating total in list btb

--- app/models/m_warehouse_in_second_d.rb
+++ app/models/m_warehouse_in_second_d.rb
@@ -1,5 +1,5 @@
 class MWarehouseInSecondD < ActiveRecord::Base
-  attr_accessible :created_at, :kode_brg, :kode_brg_ext, :kode_brg_lot, :kode_tmp, :qty, :updated_at, :satuan_id
+  attr_accessible :created_at, :kode_brg, :kode_brg_ext, :kode_brg_lot, :kode_tmp, :qty, :updated_at, :satuan_id, :harga
   belongs_to :m_warehouse_in_first_h, class_name: 'MWarehouseInFirstH', foreign_key: 'kode_tmp'
   belongs_to :satuan, class_name: 'ASatuan', foreign_key: 'satuan_id'
 end
