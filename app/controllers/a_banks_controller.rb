74ee3a0530d7115f23bd15950b6402fa10e59d68
Yoga Hapriana <thenelse@rocketmail.com>
2013-01-26 01:52:02 +0700

    Create page for listing data bank

--- app/controllers/a_banks_controller.rb
+++ app/controllers/a_banks_controller.rb
@@ -1,83 +1,19 @@
 class ABanksController < ApplicationController
-  # GET /a_banks
-  # GET /a_banks.json
+  before_filter :get_miscellaneous
+
+  @@title = 'Bank'
+  @@table_name = ABank.table_name
+
   def index
-    @a_banks = ABank.all
-
+    @banks = ABank.page(params[:page]).per(5)
     respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @a_banks }
+      format.html
     end
   end
 
-  # GET /a_banks/1
-  # GET /a_banks/1.json
-  def show
-    @a_bank = ABank.find(params[:id])
+private
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @a_bank }
-    end
-  end
-
-  # GET /a_banks/new
-  # GET /a_banks/new.json
-  def new
-    @a_bank = ABank.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @a_bank }
-    end
-  end
-
-  # GET /a_banks/1/edit
-  def edit
-    @a_bank = ABank.find(params[:id])
-  end
-
-  # POST /a_banks
-  # POST /a_banks.json
-  def create
-    @a_bank = ABank.new(params[:a_bank])
-
-    respond_to do |format|
-      if @a_bank.save
-        format.html { redirect_to @a_bank, notice: 'A bank was successfully created.' }
-        format.json { render json: @a_bank, status: :created, location: @a_bank }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @a_bank.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /a_banks/1
-  # PUT /a_banks/1.json
-  def update
-    @a_bank = ABank.find(params[:id])
-
-    respond_to do |format|
-      if @a_bank.update_attributes(params[:a_bank])
-        format.html { redirect_to @a_bank, notice: 'A bank was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @a_bank.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /a_banks/1
-  # DELETE /a_banks/1.json
-  def destroy
-    @a_bank = ABank.find(params[:id])
-    @a_bank.destroy
-
-    respond_to do |format|
-      format.html { redirect_to a_banks_url }
-      format.json { head :no_content }
-    end
+  def get_miscellaneous
+    @title = @@title
   end
 end
--- app/models/a_bank.rb
+++ app/models/a_bank.rb
@@ -1,3 +1,3 @@
 class ABank < ActiveRecord::Base
-  attr_accessible : :kode, :nama, :simbol, :st_progress, :status1, :updated_by
+  attr_accessible :kode, :nama, :simbol, :st_progress, :status1, :updated_by
 end
