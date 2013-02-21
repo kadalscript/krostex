8984ec2f82175a9a9d40c1c517231f9e5b8d64fc
Yoga Hapriana <thenelse@rocketmail.com>
2013-02-22 00:53:17 +0700

    Fix task no 9

--- app/models/b_barang.rb
+++ app/models/b_barang.rb
@@ -1,4 +1,5 @@
 class BBarang < ActiveRecord::Base
-  attr_accessible :id_department, :id_golongan, :id_level_five, :id_level_four, :id_satuan, :id_type, :kode, :max_stok, :min_stok, :nama, :reorder_stok, :updated_by
-  has_many :supplier_history_quotation, class_name: 'BSupplierHistoryQuotation', foreign_key: 'kode_brg'
+	set_table_name :b_barang_as
+	attr_accessible :id_department, :id_golongan, :id_level_five, :id_level_four, :id_satuan, :id_type, :kode, :max_stok, :min_stok, :nama, :reorder_stok, :updated_by
+	has_many :supplier_history_quotation, class_name: 'BSupplierHistoryQuotation', foreign_key: 'kode_brg'
 end
